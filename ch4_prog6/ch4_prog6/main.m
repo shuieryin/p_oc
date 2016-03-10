5f922285e29e33fc01499daac57730aa86b078e3
CollinGuo <shuieryin@gmail.com>
2016-03-10 22:40:18 +0800

    add ch4 programs

---
+++ ch4_prog6/ch4_prog6/main.m
@@ -0,0 +1,82 @@
+//
+//  main.m
+//  ch4_prog6
+//
+//  Created by GuoCollin on 3/10/16.
+//  Copyright (c) 2016 Shuieryin. All rights reserved.
+//
+
+// Implement a Calculator class
+
+#import <Foundation/Foundation.h>
+
+@interface Calculator : NSObject
+
+// accumulator methods
+- (void)setAccumulator:(double)value;
+
+- (void)clear;
+
+- (double)accumulator;
+
+// arithmetic methods
+- (double)add:(double)value;
+
+- (double)subtract:(double)value;
+
+- (double)multiply:(double)value;
+
+- (double)divide:(double)value;
+
+@end
+
+@implementation Calculator {
+    double accumulator;
+}
+
+- (void)setAccumulator:(double)value {
+    accumulator = value;
+}
+
+- (void)clear {
+    accumulator = 0;
+}
+
+- (double)accumulator {
+    return accumulator;
+}
+
+- (double)add:(double)value {
+    return accumulator += value;
+}
+
+- (double)subtract:(double)value {
+    return accumulator -= value;
+}
+
+- (double)multiply:(double)value {
+    return accumulator *= value;
+}
+
+- (double)divide:(double)value {
+    return accumulator /= value;
+}
+
+@end
+
+int main(int argc, const char *argv[]) {
+    @autoreleasepool {
+        Calculator *deskCalc = [[Calculator alloc] init]; // [Calculator new];
+
+        [deskCalc setAccumulator:100.0];
+        [deskCalc add:200.];
+        [deskCalc divide:15.0];
+        [deskCalc subtract:10.0];
+        [deskCalc multiply:5];
+        NSLog(@"The result is %g", [deskCalc accumulator]);
+        [deskCalc clear];
+        NSLog(@"Cleared result is %g", [deskCalc accumulator]);
+    }
+
+    return 0;
+}
